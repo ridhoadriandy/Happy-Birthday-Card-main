61091c5c558befe857d9b4f1c37e1e9068a895c4
Muhammad Ridho Adriandy <85445668+ridhoadriandy@users.noreply.github.com>
2024-09-14 21:20:35 +0700

    Update init.js

--- builder/init.js
+++ builder/init.js
@@ -1,49 +1,16 @@
-const axios = require("axios").default;
-const path = require("path");
-const fs = require("fs");
-const setPic = require("./getPic");
-const genIndex = require("./genIndex");
-const {
-  generateMarkupLocal,
-  generateMarkupRemote,
-} = require("./generateMarkup");
-
-require("dotenv").config();
-
-if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
-if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
-
-const picPath = process.env.PIC;
-const msgPath = process.env.SCROLL_MSG;
-
-//Local initialization
-const setLocalData = async () => {
-  try {
-    const pic = path.join(__dirname, "../local/", picPath);
-    let markup = "";
-    if (msgPath) {
-      const text = fs.readFileSync(path.join(__dirname, "../local/", msgPath), {
-        encoding: "utf-8",
-      });
-      markup = generateMarkupLocal(text);
-    }
-    await setPic(pic);
-    genIndex(markup);
-  } catch (e) {
-    throw new Error(e.message);
-  }
-};
-
-//Remote initialization
 const setRemoteData = async () => {
   try {
+    console.log(`Fetching image from: ${picPath}`);
     let res = await axios.get(picPath, {
       responseType: "arraybuffer",
     });
+    console.log(`Image fetched successfully`);
     const pic = res.data;
+
     let markup = "";
     if (msgPath) {
       const article = msgPath.split("/").pop();
+      console.log(`Fetching content from: https://api.telegra.ph/getPage/${article}?return_content=true`);
       res = await axios.get(
         `https://api.telegra.ph/getPage/${article}?return_content=true`
       );
@@ -52,14 +19,13 @@ const setRemoteData = async () => {
         (string, node) => string + generateMarkupRemote(node),
         ""
       );
+      console.log(`Content fetched and markup generated`);
     }
+
     await setPic(pic);
     genIndex(markup);
   } catch (e) {
+    console.error('Error:', e.message);
     throw new Error(e.message);
   }
 };
-
-if (process.argv[2] === "--local") setLocalData();
-else if (process.argv[2] === "--remote") setRemoteData();
-else console.log("Fetch mode not specified.");
